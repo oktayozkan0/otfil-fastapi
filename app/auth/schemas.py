ff35107ce69e5850a4f626c19f5f405f0fc85ac1
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2024-07-29 01:25:51 +0300

    user auth

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -1,7 +1,13 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class UserSignupRequest(BaseModel):
     email: EmailStr
-    username: str = Field(min_length=5, max_length=25)
+    username: str = Field(..., min_length=5, max_length=25)
     password: str
+
+    @field_validator("username")
+    def reject_email(cls, v):
+        if "@" in v:
+            raise ValueError("Username can not contain '@' symbol")
+        return v
