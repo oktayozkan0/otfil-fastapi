11b99cdb29e031075b0be073237cdf7660c71756
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2024-07-29 04:31:03 +0300

    async redis

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,18 +1,17 @@
-import redis
-import json
+from redis.asyncio import Redis
 
 from core.config import get_app_settings
 
 
 settings = get_app_settings()
 
-redis_conn = redis.from_url(settings.redis_url.unicode_string())
+redis_conn = Redis.from_url(settings.redis_url.unicode_string())
 
 async def get_user_refresh_token(email: str) -> str:
     key = f"refresh_token:{email}"
-    token = redis_conn.get(key)
+    token = await redis_conn.get(key)
     return token
 
 async def set_user_refresh_token(token: str, email: str) -> None:
     key = f"refresh_token:{email}"
-    redis_conn.set(key, token, ex=settings.refresh_token_expire_minutes*60)
+    await redis_conn.set(key, token, ex=settings.refresh_token_expire_minutes*60)
