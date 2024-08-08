a051269aa4293e969670b607167ec20989c4a422
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2024-08-09 01:29:57 +0300

    better docs

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,9 @@ from pydantic import BaseModel, PostgresDsn, RedisDsn
 DEFAULTS = {
     "openapi_url": "/openapi.json",
     "docs_url": "/docs",
-    "redoc_url": "/redoc"
+    "redoc_url": "/redoc",
+    "title": "OTFIL API",
+    "summary": "OTFIL API Swagger UI"
 }
 
 class AppSettings(BaseModel):
@@ -14,6 +16,8 @@ class AppSettings(BaseModel):
     openapi_url: str = None
     docs_url: str = None
     redoc_url: str = None
+    title: str = None
+    summary: str = None
     jwt_secret_key: str = "SUPER_SECRET_KEY"
     jwt_refresh_secret_key: str = "SUPER_SECRET_REFRESH_KEY"
     access_token_expire_minutes: int = 30
@@ -24,7 +28,5 @@ def get_app_settings() -> AppSettings:
     config = {k.lower():v for k,v in dotenv_values().items()}
     settings = AppSettings(**config)
     if settings.debug:
-        settings.openapi_url = DEFAULTS.get("openapi_url")
-        settings.docs_url = DEFAULTS.get("docs_url")
-        settings.redoc_url = DEFAULTS.get("redoc_url")
+        settings.__dict__.update(DEFAULTS)
     return settings
